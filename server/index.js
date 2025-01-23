b50ae2e9b22d914266a6e6b8873287e1d7db4175
airbornesd <dhaka.siddharth1999@gmail.com>
2025-01-23 14:17:32 +0530

    v.0.1

--- server/index.js
+++ server/index.js
@@ -1,24 +1,41 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config({ path: "../.env" });
 
 const app = express();
 const port = process.env.PORT || 7000;
+const botToken = process.env.BOT_TOKEN;
+const url = "https://api.telegram.org/bot" + botToken;
+const groupId = process.env.GROUP_ID;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/telegram", (req, res) => {
+app.get("/telegram", async (req, res) => {
   try {
-    const body = req.body;
-    const query = req.query;
-    const params = req.params;
+    const { id } = req.query;
 
-    console.log("🚀 ~ app.post");
-    res.status(200).json({ message: "success", data: { body, query, params } });
+    if (!id) return res.status(400).json({ message: "user id required" });
+
+    const {
+      data: { result },
+    } = await axios.get(url + "/getChatMember", {
+      params: {
+        chat_id: groupId,
+        user_id: id,
+      },
+    });
+
+    res.status(200).json({ message: "success", data: result });
   } catch (error) {
-    console.error("🚀 ~ app.post ~ error:", error);
+    if (error.response)
+      return res
+        .status(error.response.status)
+        .json({ message: error.response.data.description });
+
     res.status(500).json({ message: error.message });
   }
 });
